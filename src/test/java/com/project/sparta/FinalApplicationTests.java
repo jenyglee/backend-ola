7ff4d9120cd7fe323aa23008455f53f750738403
이재원 <jenyglee_30@naver.com>
2023-02-05 19:58:59 +0900

    Feat: Querydsl 세팅 적용
    그래들에 querydsl 세팅 적용 및 테스트
    
    Fixes: #0-1

--- src/test/java/com/project/sparta/FinalApplicationTests.java
+++ src/test/java/com/project/sparta/FinalApplicationTests.java
@@ -1,6 +1,9 @@
 package com.project.sparta;
 
 import com.project.sparta.entity.Hello;
+import com.project.sparta.entity.QHello;
+import com.querydsl.jpa.JPQLQueryFactory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +19,21 @@ class FinalApplicationTests {
     EntityManager em;
     @Test
     void contextLoads() {
+        //기본 세팅 테스트
+        // Hello hello = new Hello();
+        // em.persist(hello);
+        //
+        // Hello hello1 = em.find(Hello.class, hello.getId());
+        // Assertions.assertThat(hello).isEqualTo(hello1);
+
+        //querydsl 세팅 테스트
         Hello hello = new Hello();
         em.persist(hello);
+        JPQLQueryFactory queryFactory = new JPAQueryFactory(em);
 
-        Hello hello1 = em.find(Hello.class, hello.getId());
+        Hello hello1 = queryFactory.selectFrom(QHello.hello)
+                .fetchOne();
+
         Assertions.assertThat(hello).isEqualTo(hello1);
     }
 
