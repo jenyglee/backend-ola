bcef385655a5f58a41a8f0cc393233b77a4a4a74
이재원 <jenyglee_30@naver.com>
2023-02-05 19:46:06 +0900

    Feat: 프로젝트 실행 테스트
    임시로 Hello 엔티티를 만들어 프로젝트 실행과 테스트코드 실행을 테스트함.
    
    Fixes: #0-1

--- src/test/java/com/project/sparta/FinalApplicationTests.java
+++ src/test/java/com/project/sparta/FinalApplicationTests.java
@@ -1,13 +1,26 @@
 package com.project.sparta;
 
+import com.project.sparta.entity.Hello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class FinalApplicationTests {
-
+    @Autowired
+    EntityManager em;
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        Hello hello1 = em.find(Hello.class, hello.getId());
+        Assertions.assertThat(hello).isEqualTo(hello1);
     }
 
 }
