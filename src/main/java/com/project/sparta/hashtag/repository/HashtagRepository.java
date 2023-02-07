e53f0c9cc8a71321c834c454a1bcc5a78089ac5e
이재원 <jenyglee_30@naver.com>
2023-02-07 19:23:12 +0900

    Fix: 테스트에러 디버깅 진행중
    No qualifying bean of type 'com.project.sparta.user.repository.UserRepository' available: expected at least 1 bean which qualifies as autowire candidate. Dependency annotations: {}

--- src/main/java/com/project/sparta/hashtag/repository/HashtagRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.sparta.hashtag.repository;
-
-import com.project.sparta.hashtag.entity.Hashtag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HashtagRepository extends JpaRepository<Hashtag,Long> {
-
-}
--- src/main/java/com/project/sparta/hashtag/service/HashtagService.java
+++
@@ -1,8 +0,0 @@
-package com.project.sparta.hashtag.service;
-
-import com.project.sparta.hashtag.entity.Hashtag;
-import com.project.sparta.user.entity.User;
-
-public interface HashtagService {
-    Hashtag createHashtag(String value, User user);
-}
