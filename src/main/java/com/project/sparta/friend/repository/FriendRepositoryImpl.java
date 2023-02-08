7469fbaa820ae91d2d0efc8b5c1121455bd484f2
seinHan <seinhan151@gmail.com>
2023-02-08 10:09:11 +0900

    [#5-3] Fix : test 버그 수정

--- src/main/java/com/project/sparta/friend/repository/FriendRepositoryImpl.java
+++ src/main/java/com/project/sparta/friend/repository/FriendRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.project.sparta.friend.repository;
 
-import com.project.sparta.user.dto.UserResponseDto;
 import com.project.sparta.user.entity.QUser;
+import com.project.sparta.user.entity.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @RequiredArgsConstructor
 public class FriendRepositoryImpl implements FriendCustomRepository {
@@ -13,4 +15,26 @@ public class FriendRepositoryImpl implements FriendCustomRepository {
 
     QUser user = new QUser("user");
 
+
+    /*
+    * @Override
+    public PageImpl<Board> getAll(Pageable pageable) {
+        List<Board> boardList = queryFactory.select(board)
+                .from(board)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        return new PageImpl<>(boardList, pageable, boardList.size());
+    }
+    */
+
+    @Override
+    public Page<User> serachFriend(String targetUserName, Pageable pageRequest) {
+//        return queryFactory.select(user)
+//                .from(user)
+//                .where(user.userName.eq(targetUserName))
+//                .offset()
+
+        return null;
+    }
 }
--- src/test/java/com/project/sparta/member/service/UserServiceImplTest.java
+++ src/test/java/com/project/sparta/member/service/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package com.project.sparta.member.service;
 
 import com.project.sparta.user.entity.QUser;
 import com.project.sparta.user.entity.User;
+import com.project.sparta.user.entity.UserRoleEnum;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,10 +34,11 @@ class UserServiceImplTest {
 
     @BeforeEach
     public void Befor() {
-        User user1 = new User("1234", "user1", 10, "010-1234-1234", "user1@naver.com", USER);
-        User user2 = new User("1234", "user2", 20, "010-1234-1235", "user2@naver.com", USER);
-        User user3 = new User("1234", "user3", 30, "010-1234-1236", "user3@naver.com", USER);
-        User user4 = new User("1234", "user4", 40, "010-1234-1237", "user4@naver.com", ADMIN);
+        User user1 = new User("1234", "이재원", 10, "010-1234-1234", "user1@naver.com", UserRoleEnum.USER, "user1.jpg","Y");
+        User user2 = new User("1234", "한세인", 20, "010-1234-1234", "user2@naver.com", UserRoleEnum.USER, "user2.jpg","Y");
+        User user3 = new User("1234", "김민선", 30, "010-1234-1234", "user3@naver.com", UserRoleEnum.USER, "user3.jpg","Y");
+        User user4 = new User("1234", "김주성", 20, "010-1234-1234", "user4@naver.com", UserRoleEnum.USER, "user4.jpg","Y");
+        User user5 = new User("1234", "김두영", 30, "010-1234-1234", "user5@naver.com", UserRoleEnum.USER, "user5.jpg","Y");
         em.persist(user1);
         em.persist(user2);
         em.persist(user3);
@@ -51,9 +53,9 @@ class UserServiceImplTest {
     public void findMemberQueryDsl() {
         User findUser = queryFactory
                                 .selectFrom(user)
-                                .where(user.username.eq("user1"))
+                                .where(user.userName.eq("user1"))
                                 .fetchOne();
 
-        assertThat(findUser.getUsername()).isEqualTo("user1");
+        assertThat(findUser.getUserName()).isEqualTo("user1");
     }
 }
