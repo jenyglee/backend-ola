769f9affcf75be14533b2effcbea45eaf25806e9
seinHan <seinhan151@gmail.com>
2023-02-08 15:42:25 +0900

    Test : 친구 추가/삭제/검색 test 완료

--- src/main/java/com/project/sparta/friend/service/FriendService.java
+++ src/main/java/com/project/sparta/friend/service/FriendService.java
@@ -1,13 +1,15 @@
 package com.project.sparta.friend.service;
 
 
-import com.project.sparta.friend.dto.FriendSearchResultDto;
-import com.project.sparta.user.dto.UserResponseDto;
+import com.project.sparta.common.dto.PageResponseDto;
+import com.project.sparta.friend.dto.FriendSearchReponseDto;
+
+import java.util.List;
 
 public interface FriendService {
 
     void addFriend(Long userId, String targetUsername); //친구 추가
     void deleteFriend(String targetUsername);    // 친구 삭제
-    FriendSearchResultDto searchFriend(int offset, int limit, String targetUsername);    //친구 검색
+    PageResponseDto<List<FriendSearchReponseDto>> searchFriend(int offset, int limit, String targetUsername);    //친구 검색
 
 }
