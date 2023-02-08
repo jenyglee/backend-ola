a8f64e6fe223845e39e53db604277540b5ed1d50
이재원 <jenyglee_30@naver.com>
2023-02-08 11:02:19 +0900

    Feat: 해시태그 전체조회 (페이징 처리) 완료, 확장의 유연성을 위한 PageResponseDto 추가

--- src/test/java/com/project/sparta/hashtag/service/HashtagServiceImplTest.java
+++ src/test/java/com/project/sparta/hashtag/service/HashtagServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.project.sparta.hashtag.service;
 
+import com.project.sparta.common.dto.PageResponseDto;
+import com.project.sparta.hashtag.dto.HashtagResponseDto;
 import com.project.sparta.hashtag.entity.Hashtag;
 import com.project.sparta.hashtag.repository.HashtagRepository;
 import com.project.sparta.user.entity.User;
@@ -25,6 +27,7 @@ class HashtagServiceImplTest {
     @Test
     public void createHashtag() {
         User user = new User("1234", "이재원", 10, "010-1234-1234", "user1@naver.com", UserRoleEnum.USER, "user1.jpg","Y");
+
         Hashtag saved = hashtagService.createHashtag("등린이", user);
 
         assertThat(saved.getName()).isEqualTo("등린이");
@@ -33,25 +36,27 @@ class HashtagServiceImplTest {
     @Test
     public void deleteHashtag() {
         User user = new User("1234", "이재원", 10, "010-1234-1234", "user1@naver.com", UserRoleEnum.USER, "user1.jpg","Y");
-        Hashtag saved = hashtagService.createHashtag("등린이", user);
+        Hashtag saved = hashtagRepository.save(new Hashtag("등린이"));
+
         hashtagService.deleteHashtag(saved.getId(), user);
 
         List<Hashtag> allHashtag = hashtagRepository.findAll();
-
         assertThat(allHashtag.size()).isEqualTo(0);
-
-
         assertThat(saved.getName()).isEqualTo("등린이");
     }
 
     @Test
     public void getHashtagList(){
+        hashtagRepository.save(new Hashtag("등린이"));
+        hashtagRepository.save(new Hashtag("금강산"));
+        hashtagRepository.save(new Hashtag("식후경"));
+        hashtagRepository.save(new Hashtag("야호"));
+
         User user = new User("1234", "이재원", 10, "010-1234-1234", "user1@naver.com", UserRoleEnum.USER, "user1.jpg","Y");
-        hashtagService.createHashtag("등린이", user);
+        PageResponseDto<List<HashtagResponseDto>> hashtagList = hashtagService.getHashtagList(0, 3, user);
 
-        List<Hashtag> allHashtag = hashtagRepository.findAll();
-        assertThat(allHashtag.size()).isEqualTo(1);
-
-        // 해시태그에 페이징 필요
+        assertThat(hashtagList.getCurrentPage()).isEqualTo(0);
+        assertThat(hashtagList.getData().size()).isEqualTo(3);
+        assertThat(hashtagList.getTotalCount()).isEqualTo(4);
     }
 }
