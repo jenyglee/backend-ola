5d9259b9e026f5e81cd6f7511b7d910533e92c7c
이재원 <jenyglee_30@naver.com>
2023-02-12 13:27:06 +0900

    Feat: 공지사항 API제작 및 엔티티 필드 수정

--- back/src/main/java/com/project/sparta/noticeBoard/entity/NoticeBoard.java
+++ back/src/main/java/com/project/sparta/noticeBoard/entity/NoticeBoard.java
@@ -2,11 +2,14 @@ package com.project.sparta.noticeBoard.entity;
 
 import com.project.sparta.admin.entity.Timestamped;
 import com.project.sparta.noticeBoard.dto.NoticeBoardRequestDto;
+import com.project.sparta.user.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 @NoArgsConstructor
@@ -17,23 +20,29 @@ public class NoticeBoard extends Timestamped {
     @Column(name ="noticeBoard_id")
     private Long id;
 
-    private String nickName;
+    @ManyToOne(fetch = LAZY)
+    private User user;
 
     @Column(nullable = false)
     private String title;
     @Column(nullable = false)
     private String contents;
 
+    @Column(nullable = false)
+    private NoticeCategoryEnum category;
 
-    public NoticeBoard(String nickName, String title, String contents) {
-        this.nickName = nickName;
+
+    public NoticeBoard(User user, String title, String contents, NoticeCategoryEnum category) {
+        this.user = user;
         this.title = title;
         this.contents = contents;
+        this.category = category;
     }
 
-    public void update(String title, String contents){
+    public void update(String title, String contents, NoticeCategoryEnum category){
         this.title = title;
         this.contents = contents;
+        this.category = category;
     }
 
 }
--- back/src/main/java/com/project/sparta/noticeBoard/service/NoticeBoardServiceImpl.java
+++ back/src/main/java/com/project/sparta/noticeBoard/service/NoticeBoardServiceImpl.java
@@ -27,53 +27,46 @@ public class NoticeBoardServiceImpl implements NoticeBoardService {
 
     //공지글 작성
     @Override
-    public NoticeBoardResponseDto createNoticeBoard(NoticeBoardRequestDto noticeBoardRequestDto, String nickName) {
-
-        NoticeBoard noticeBoard = new NoticeBoard(noticeBoardRequestDto.getTitle(), noticeBoardRequestDto.getContents(), nickName);
-
+    public void createNoticeBoard(NoticeBoardRequestDto requestDto, User user) {
+        // 0: SERVICE, 1: UPDATE, 2: EVENT
+        NoticeBoard noticeBoard = new NoticeBoard(user, requestDto.getTitle(), requestDto.getContents(), requestDto.getCategory());
         noticeBoardRepository.saveAndFlush(noticeBoard);
-
-        NoticeBoard resultNotice = noticeBoardRepository.findById(noticeBoard.getId()).orElseThrow(() -> new CustomException(INVALID_USER));
-
-        return new NoticeBoardResponseDto(resultNotice.getId(), resultNotice.getNickName(), resultNotice.getTitle(),
-                resultNotice.getContents(), resultNotice.getModifiedAt(), resultNotice.getCreateAt());
     }
 
     //공지글 삭제
     @Override
-    public void deleteNoticeBoard(Long noticeId) {
-        NoticeBoard noticeBoard = noticeBoardRepository.findById(noticeId).orElseThrow(() -> new CustomException(NOT_FOUND_POST));
+    public void deleteNoticeBoard(Long id, User user) {
+        NoticeBoard noticeBoard = noticeBoardRepository.findByIdAndUser_NickName(id, user.getNickName()).orElseThrow(() -> new CustomException(NOT_FOUND_POST));
         noticeBoardRepository.delete(noticeBoard);
     }
 
     //공지글 수정
     @Override
-    public void updateNoticeBoard(NoticeBoardRequestDto noticeBoardRequestDto, Long noticeId) {
-        NoticeBoard noticeBoard = noticeBoardRepository.findById(noticeId).orElseThrow(() -> new CustomException(NOT_FOUND_POST));
-        noticeBoard.update(noticeBoardRequestDto.getTitle(), noticeBoardRequestDto.getContents());
+    public void updateNoticeBoard(Long id, NoticeBoardRequestDto requestDto, User user) {
+        NoticeBoard noticeBoard = noticeBoardRepository.findByIdAndUser_NickName(id, user.getNickName()).orElseThrow(() -> new CustomException(NOT_FOUND_POST));
+        noticeBoard.update(requestDto.getTitle(), requestDto.getContents(), requestDto.getCategory());
     }
 
     //공지글 단건 조회
     @Override
-    public NoticeBoardResponseDto getNoticeBoard(Long noticeId) {
+    public NoticeBoardResponseDto getNoticeBoard(Long id, User user) {
 
-        NoticeBoard noticeBoard = noticeBoardRepository.findById(noticeId).orElseThrow(() -> new CustomException(NOT_FOUND_POST));
+        NoticeBoard noticeBoard = noticeBoardRepository.findById(id).orElseThrow(() -> new CustomException(NOT_FOUND_POST));
 
-        return new NoticeBoardResponseDto(noticeBoard.getId(), noticeBoard.getNickName(), noticeBoard.getTitle(),
-                noticeBoard.getContents(), noticeBoard.getModifiedAt(), noticeBoard.getCreateAt());
+        return new NoticeBoardResponseDto(noticeBoard.getId(), noticeBoard.getUser().getNickName(), noticeBoard.getTitle(),
+                noticeBoard.getContents(), noticeBoard.getCategory(), noticeBoard.getModifiedAt(), noticeBoard.getCreateAt());
     }
 
     //공지글 전체조회
     @Override
     public PageResponseDto<List<NoticeBoardResponseDto>> getAllNoticeBoard(int offset, int limit, User user) {
-
         // 1. 페이징으로 요청해서 조회
         PageRequest pageRequest = PageRequest.of(offset, limit);
         Page<NoticeBoard> results = noticeBoardRepository.findAll(pageRequest);
 
         // 2. 데이터, 전체 개수 추출
-        Page<NoticeBoardResponseDto> allList = results.map(noticeBoard -> new NoticeBoardResponseDto(noticeBoard.getId(), noticeBoard.getNickName(), noticeBoard.getTitle(),
-                noticeBoard.getContents(), noticeBoard.getModifiedAt(), noticeBoard.getCreateAt()));
+        Page<NoticeBoardResponseDto> allList = results.map(noticeBoard -> new NoticeBoardResponseDto(noticeBoard.getId(), noticeBoard.getUser().getNickName(), noticeBoard.getTitle(),
+                noticeBoard.getContents(), noticeBoard.getCategory(), noticeBoard.getModifiedAt(), noticeBoard.getCreateAt()));
 
         List<NoticeBoardResponseDto> content = allList.getContent();
         long totalElements = allList.getTotalElements();
