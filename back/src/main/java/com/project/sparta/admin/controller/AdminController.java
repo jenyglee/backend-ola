7ed78907d17d2395f893e8906c63211a1609ffac
jusung kim <karlema8900@gmail.com>
2023-02-13 13:56:33 +0900

    Feat : ADMIN 파일 구조 변경

--- back/src/main/java/com/project/sparta/admin/controller/AdminController.java
+++ back/src/main/java/com/project/sparta/admin/controller/AdminController.java
@@ -2,23 +2,38 @@ package com.project.sparta.admin.controller;
 
 
 import com.project.sparta.admin.dto.AdminSignupDto;
+import com.project.sparta.communityBoard.dto.CommunityBoardRequestDto;
+import com.project.sparta.communityBoard.dto.CommunityBoardResponseDto;
+import com.project.sparta.communityBoard.service.CommunityBoardService;
+import com.project.sparta.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RestController;
-import com.project.sparta.admin.service.AdminService;
+import com.project.sparta.admin.service.AdminServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AdminController {
-    private final AdminService adminService;
+    private final AdminServiceImpl adminServiceImpl;
 
+    private final CommunityBoardService communityBoardService;
     // 어드민 회원가입
     @PostMapping("/signup/admin")
     public ResponseEntity signup(@RequestBody AdminSignupDto signupDto){
-        adminService.signup(signupDto);
+        adminServiceImpl.signup(signupDto);
         return new ResponseEntity(HttpStatus.OK);
     }
+    @PatchMapping("/admin_community_boards/{community_board_id}")
+    public ResponseEntity updateCommunityBoard(@PathVariable Long community_board_id, @RequestBody CommunityBoardRequestDto communityBoardRequestDto
+        ,@AuthenticationPrincipal UserDetailsImpl userDetail) {
+        CommunityBoardResponseDto communityBoardResponseDto = communityBoardService.updateCommunityBoard(
+            community_board_id, communityBoardRequestDto, userDetail.getUser());
+        return new ResponseEntity<>(communityBoardResponseDto, HttpStatus.OK);
+    }
+
+
 }
