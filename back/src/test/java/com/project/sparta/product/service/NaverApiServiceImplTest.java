298a3c96d533863c92903b96b36068248ed24391
이재원 <jenyglee_30@naver.com>
2023-02-16 18:25:27 +0900

    Fix : 테스트코드 전체 주석

--- back/src/test/java/com/project/sparta/product/service/NaverApiServiceImplTest.java
+++ back/src/test/java/com/project/sparta/product/service/NaverApiServiceImplTest.java
@@ -1,32 +1,32 @@
-package com.project.sparta.product.service;
-
-import com.project.sparta.common.dto.PageResponseDto;
-import com.project.sparta.product.dto.ProductResponseDto;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-@Transactional
-class NaverApiServiceImplTest {
-    // @Autowired
-    // NaverApiService naverApiService;
-    //
-    // @Test
-    // void searchItems() {
-    //     PageResponseDto<List<ProductResponseDto>> results = naverApiService.searchItems("macbook", 30, 1, "dsc");
-    //     List<ProductResponseDto> data = results.getData();
-    //     int currentPage = results.getCurrentPage();
-    //
-    //     assertThat(data.size()).isEqualTo(30);
-    //     assertThat(currentPage).isEqualTo(1);
-    // }
-
-}
+// package com.project.sparta.product.service;
+//
+// import com.project.sparta.common.dto.PageResponseDto;
+// import com.project.sparta.product.dto.ProductResponseDto;
+// import org.assertj.core.api.Assertions;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.transaction.annotation.Transactional;
+//
+// import java.util.List;
+//
+// import static org.assertj.core.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
+//
+// @SpringBootTest
+// @Transactional
+// class NaverApiServiceImplTest {
+//     // @Autowired
+//     // NaverApiService naverApiService;
+//     //
+//     // @Test
+//     // void searchItems() {
+//     //     PageResponseDto<List<ProductResponseDto>> results = naverApiService.searchItems("macbook", 30, 1, "dsc");
+//     //     List<ProductResponseDto> data = results.getData();
+//     //     int currentPage = results.getCurrentPage();
+//     //
+//     //     assertThat(data.size()).isEqualTo(30);
+//     //     assertThat(currentPage).isEqualTo(1);
+//     // }
+//
+// }
