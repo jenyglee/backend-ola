f88476ab5acc8ce300a9909cc8fe135a8c1a0a3c
seinHan <seinhan151@gmail.com>
2023-02-17 10:52:56 +0900

    Feat : 실시간 채팅 기능 구현중

--- back/src/main/java/com/project/sparta/chat/controller/ChatController.java
+++ back/src/main/java/com/project/sparta/chat/controller/ChatController.java
@@ -1,20 +1,17 @@
 package com.project.sparta.chat.controller;
 
 
+import com.project.sparta.chat.repository.ChatRepository;
 import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-@Log4j2
+@Slf4j
 public class ChatController {
 
-    @GetMapping("/chat")
-    public String chatGET(){
-
-        log.info("@ChatController, chat GET()");
-
-        return "chat";
-    }
+    //ChatRepostiory Bean 가져오기
+   private ChatRepository chatRepository;
 }
--- back/src/main/java/com/project/sparta/config/WebSocketConfig.java
+++ back/src/main/java/com/project/sparta/config/WebSocketConfig.java
@@ -1,23 +1,30 @@
 package com.project.sparta.config;
 
 
-import com.project.sparta.chat.ChatHandler;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final ChatHandler chatHandler;
+@EnableWebSocketMessageBroker // 문자 채팅용
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
+    //웹 소켓 연결을 위한 엔드포인트 설정 및 stomp sub/pub 엔트 포인트 설정
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //stomp 접속 주소 url => /ws-stomp
+        registry.addEndpoint("/ws-stomp")//연결될 엔드 포인트
+                .withSockJS();  //SocketJs를 연결한다는 설정
+
+    }
+
+
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //메세지 구독 요청 url => 즉 메세지 받을때
+        registry.enableSimpleBroker("/sub");
+
+        //메세지를 발행하는 요청 url => 즉 메세지를 보낼 때
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 }
