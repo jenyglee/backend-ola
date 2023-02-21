93870b2e720c4e99703d435337130375c6d5d504
jusung kim <karlema8900@gmail.com>
2023-02-21 11:12:26 +0900

    Feat : 수정중

--- back/src/main/java/com/project/sparta/config/WebConfig.java
+++ back/src/main/java/com/project/sparta/config/WebConfig.java
@@ -1,5 +1,4 @@
 package com.project.sparta.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,16 +6,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-<<<<<<< HEAD
-     @Override
-     public void addCorsMappings(CorsRegistry registry) {
-       registry.addMapping("/**")
-           .allowedOrigins("*")
-           .allowedMethods("*")
-           .allowedOriginPatterns("*")
-           .exposedHeaders("Authorization");
-=======
->>>>>>> 959da9d67e147d2cd88e72cd6275091a684acfdf
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -30,7 +19,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/swagger-ui/**")
             .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/2.6.1/");
-
     }
 }
 
