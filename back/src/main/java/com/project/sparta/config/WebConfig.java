af20cc3dde456717baab1bc4b84f43eed896bb91
seinHan <seinhan151@gmail.com>
2023-02-21 18:15:39 +0900

    Feat : JwtFilter CORS에러 SecurityConfig(내용추가), WebConfig(수정)

--- back/src/main/java/com/project/sparta/config/WebConfig.java
+++ back/src/main/java/com/project/sparta/config/WebConfig.java
@@ -1,6 +1,12 @@
 package com.project.sparta.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,21 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
 
      @Override
      public void addCorsMappings(CorsRegistry registry) {
-       registry.addMapping("/**")
-           .allowedOrigins("*")
-           .allowedMethods("*")
-           .allowedOriginPatterns("*")
-           .exposedHeaders("Authorization");
+
+         registry.addMapping("/**")      //패턴
+             .allowedOrigins("*")    //URL
+             .allowedOrigins("http://localhost:8080","http://localhost:63342") //URL
+             .allowedHeaders("Authorization", "Content-type")
+             .exposedHeaders("Authorization", "Content-Disposition")
+             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");        //method
      }
-
-
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//            //.allowedOrigins("http://localhost:63342/")
-//            .allowedOrigins("*")
-//            .exposedHeaders("Authorization");
-//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
