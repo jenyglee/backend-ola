d86d948f319cee9639c73bd8d3432accdccb79ee
seinHan <seinhan151@gmail.com>
2023-02-23 01:08:32 +0900

    Feat : 친구 검색 front 작업 중

--- back/src/main/java/com/project/sparta/user/repository/UserRepository.java
+++ back/src/main/java/com/project/sparta/user/repository/UserRepository.java
@@ -6,10 +6,14 @@ import com.project.sparta.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
     Optional<User> findByEmail(String email);
     Optional<User> findByKakaoId(Long kakaoId);
     Optional<User> findByNickName(String nickName);
-    Optional<User> findByNickNameAndStatus(String username, StatusEnum statusEnum);
+
+    @Query("select u from USERS u where u.Id=:friendId and u.status=:statusEnum")
+    Optional<User> findFriend(@Param("friendId") Long friendId, @Param("statusEnum") StatusEnum statusEnum);
 }
