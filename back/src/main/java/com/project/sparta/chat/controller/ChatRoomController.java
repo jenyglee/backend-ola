6c44b603346427f267be7da009553b7ca38d8e62
seinHan <seinHan151@gmail.com>
2023-03-05 02:45:26 +0900

    Fix : ChatController 수정 중

--- back/src/main/java/com/project/sparta/chat/controller/ChatRoomController.java
+++ back/src/main/java/com/project/sparta/chat/controller/ChatRoomController.java
@@ -2,32 +2,19 @@ package com.project.sparta.chat.controller;
 
 import static com.project.sparta.exception.api.Status.NOT_FOUND_COMMUNITY_BOARD;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
 import com.project.sparta.chat.dto.ChatRoomDto;
 import com.project.sparta.chat.dto.ChatRoomMap;
 import com.project.sparta.chat.service.ChatServiceMain;
 import com.project.sparta.communityBoard.entity.CommunityBoard;
-import com.project.sparta.communityBoard.entity.CommunityBoardImg;
 import com.project.sparta.communityBoard.repository.BoardRepository;
-import com.project.sparta.communityBoard.repository.CommunityBoardImgRepository;
-import com.project.sparta.communityBoard.service.CommunityBoardService;
-import com.project.sparta.communityComment.entity.CommunityComment;
-import com.project.sparta.communityComment.repository.CommentRepository;
 import com.project.sparta.exception.CustomException;
-import com.project.sparta.security.UserDetailsImpl;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,23 +27,18 @@ public class ChatRoomController {
     // ChatService Bean 가져오기
     private final ChatServiceMain chatServiceMain;
 
-    private final BoardRepository boardRepository;
-
     //채팅룸 상세 정보
     @GetMapping("/chat/room")
     public ResponseEntity roomDetail(@RequestParam String roomId) {
 
-        log.info("roomId {}", roomId);
-
         ChatRoomDto room = ChatRoomMap.getInstance().getChatRooms().get(roomId);
-
 
         // TODO CommunityBoardAllResponseDto에 현재 전체 참가자 수(maxUserCnt), 현재 참가자 수(userCount) 필드 추가
         // 1. 현재 참가자 수 추출
         int currentUser = room.getUserCount();
 
         //2. 해당 커뮤니티 추출
-        CommunityBoard communityBoard = boardRepository.findById(Long.parseLong(roomId))
+        CommunityBoard communityBoard = boardRepository.findById()
             .orElseThrow(() -> new CustomException(NOT_FOUND_COMMUNITY_BOARD));
 
         //3. 해당 커뮤니티에 참가자 수 입력
@@ -87,14 +69,14 @@ public class ChatRoomController {
         ChatRoomDto beforeRoom = ChatRoomMap.getInstance().getChatRooms().get(roomId);
 
         ChatRoomDto room = ChatRoomDto.builder()
-            .roomId(String.valueOf(beforeRoom.getRoomId()))
+            .roomId(beforeRoom.getRoomId())
             .roomName(beforeRoom.getRoomName())
             .hostName(beforeRoom.getHostName())
             .userCount(beforeRoom.getUserCount())
             .maxUserCnt(roomMaxCnt)
             .build();
 
-        ChatRoomMap.getInstance().getChatRooms().put(roomId, room);
+        ChatRoomMap.getInstance().getChatRooms().put(roomId), room);
 
         return new ResponseEntity(HttpStatus.OK);
     }
