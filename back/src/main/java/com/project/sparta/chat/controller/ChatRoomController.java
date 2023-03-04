4b0585546edd0cdf166bfb5c6ab93441697924f4
이재원 <jenyglee_30@naver.com>
2023-03-05 03:14:19 +0900

    Refactor : 필요없는 import 문 제거

--- back/src/main/java/com/project/sparta/chat/controller/ChatRoomController.java
+++ back/src/main/java/com/project/sparta/chat/controller/ChatRoomController.java
@@ -1,33 +1,15 @@
 package com.project.sparta.chat.controller;
 
-import static com.project.sparta.exception.api.Status.NOT_FOUND_COMMUNITY_BOARD;
-
 import com.project.sparta.chat.dto.ChatRoomDto;
 import com.project.sparta.chat.dto.ChatRoomMap;
 import com.project.sparta.chat.service.ChatServiceMain;
-import com.project.sparta.communityBoard.entity.CommunityBoard;
-import com.project.sparta.communityBoard.entity.CommunityBoardImg;
 import com.project.sparta.communityBoard.repository.BoardRepository;
-import com.project.sparta.communityBoard.repository.CommunityBoardImgRepository;
-import com.project.sparta.communityBoard.service.CommunityBoardService;
-import com.project.sparta.communityComment.entity.CommunityComment;
-import com.project.sparta.communityComment.repository.CommentRepository;
-import com.project.sparta.exception.CustomException;
-import com.project.sparta.security.UserDetailsImpl;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
