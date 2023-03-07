2aa3ed3c70efa2b372e0797967bfe82fe5b75af3
minseon33 <109520441+minseon33@users.noreply.github.com>
2023-03-08 03:12:41 +0900

    Feat: 코멘트 스웨거 완료

--- back/src/main/java/com/project/sparta/communityComment/dto/CommunityRequestDto.java
+++ back/src/main/java/com/project/sparta/communityComment/dto/CommunityRequestDto.java
@@ -1,5 +1,6 @@
 package com.project.sparta.communityComment.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CommunityRequestDto {
 
+  @ApiModelProperty(example = "스웨거로 전달할 내용")
   private String contents;
 
   public CommunityRequestDto(String contents) {
