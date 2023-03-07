4853045a0fce9fb316415d4a7c91fbc25a986196
DUYOUNGKIM <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-03-07 22:44:06 +0900

    Feat: 친구,채팅,알람 스웨거 수정

--- back/src/main/java/com/project/sparta/chat/dto/ChatRequestDto.java
+++ back/src/main/java/com/project/sparta/chat/dto/ChatRequestDto.java
@@ -1,5 +1,6 @@
 package com.project.sparta.chat.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,12 +9,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class ChatRequestDto {
-
+    @ApiModelProperty(example = "1")
     private Long roomId;
+    @ApiModelProperty(example = "스웨거로 전달할 타이틀")
 
     private String title;
-
+    @ApiModelProperty(example = "Y")
     private String chatStatus;
 
+    @ApiModelProperty(example = "5")
     private int chatMemCnt;
 }
